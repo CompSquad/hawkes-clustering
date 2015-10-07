63ac2a6a643d055d9394601a1df4a258f0611d99
Arnaud Rachez <arnaud.rachez@gmail.com>
2015-10-07 15:31:39 +0200

    Added Q(theta, theta_old)

--- hawkes/hawkes.py
+++ hawkes/hawkes.py
@@ -45,6 +45,41 @@ def StructureProbMatrix(dt, b):
     p /= np.sum(p, axis=1)[np.newaxis].T
     return p
 
+def Q(mu, a, b, t, p, dt):
+    """ Computes Q(theta, theta_old).
+    
+    Note
+    ----
+    The theta_old parameter are implicit in dt.
+    
+    Parameters
+    ----------
+    mu : float
+        New param.
+    a : float
+        New param.
+    b : float
+        New param.
+    
+    t : array-like
+        Times of events.
+    p : ndarray
+        2D array of parenthood probabilities.
+    dt : ndarray
+        Time differences.
+    
+    Returns
+    -------
+    q : float
+        Value of Q(theta, theta_old).
+    """
+    T = t[-1] - t[0]
+    q = np.log(mu) * np.diag(p).sum() - mu * T \
+        + (p * (np.log(a) - b * dt)).sum() \
+        + a / b * (np.exp(-b * (T - t)) - 1).sum()
+    return q
+    
+
 def f(t, T, S0, S1, S2):
     """ Function appearing in the third equation of the M-step.
     
@@ -54,17 +89,17 @@ def f(t, T, S0, S1, S2):
     """
     def fclosed(b):
         fc = S1 + b * S2 \
-             + b**2 * S1 * np.sum((T - t) * np.exp(-b * (T - t))) \
-             + np.sum(1 - np.exp(-b * (T - t)))
+             - (b**2 * S1 * np.sum((T - t) * np.exp(-b * (T - t))) /
+                np.sum(1 - np.exp(-b * (T - t))))
         return fc
     return fclosed
 
-def ExpectationMaximization(time, niter=100, mu0=1, a0=1, b0=.5, callback=None):
+def ExpectationMaximization(t, niter=100, mu0=1, a0=1, b0=.5, callback=None):
     """ Expectation-Maximization as described in Guillaume's PDF. """
     
     # Basic data structures and initialization
-    T = time[-1] - time[0]
-    dt = TDMatrix(time)
+    T = t[-1] - t[0]
+    dt = TDMatrix(t)
     mu, a, b = mu0, a0, b0
     
     for _ in xrange(niter):
@@ -79,23 +114,27 @@ def ExpectationMaximization(time, niter=100, mu0=1, a0=1, b0=.5, callback=None):
         S2 = (p * dt).sum()
         
         mu = S0 / T
-        b = newton(f(time, T, S0, S1, S2), b, tol = 0.001)
-        a = b * S1 / np.sum(1 - np.exp(-b * (T - time)))
+        b = newton(f(t, T, S0, S1, S2), b, maxiter=1000, tol=0.1)
+        a = b * S1 / np.sum(1 - np.exp(-b * (T - t)))
         
         if callback is not None:
-            callback(mu, a , b)
+            callback(mu, a , b, t, p, dt)
         
 if __name__ == "__main__":
     
     import pandas as pd
+    
     df = pd.read_csv('GAZPRU.csv', parse_dates=['TradeDateTime'], index_col='TradeDateTime')
     ts = pd.Series(df.index)
     time = sorted(ts.astype(int))
-    t = np.array(time)[:10] / 1e12
-    t -= t[0]
+    time = np.array(time)
+    time -= time[0]
+    time = time[:100]
+    time = time / float(time[-1])
     
-    def inspect(mu, a, b):
-        print mu, a, b
+    def inspect(mu, a, b, t, p, dt):
+        print ("(mu, a, b) = (%.2f, %.2f, %.2f)" % (mu, a, b)).ljust(35),
+        print "Q = %s" % Q(mu, a, b, t, p, dt)
         
-    ExpectationMaximization(t, 100, callback=inspect)
+    ExpectationMaximization(time, 100, callback=inspect)
     
