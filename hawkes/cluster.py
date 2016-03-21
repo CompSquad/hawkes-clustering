8fabaf43008e832145bdd861b1fb51236b743dac
Arnaud Rachez <arnaud.rachez@gmail.com>
2016-03-21 15:47:50 +0100

    New database.

--- hawkes/cluster.py
+++ hawkes/cluster.py
@@ -8,7 +8,7 @@ from scipy.sparse import csr_matrix, coo_matrix
 
 
 def FormatAndSplit(df, cut_date):
-    """ Preprocess data into Customer, Ticker, Rating dataframe.
+    """ Preprocess data into CustomerNameIdx, Ticker, Rating dataframe.
     
     Arguments
     ---------
@@ -22,44 +22,46 @@ def FormatAndSplit(df, cut_date):
     A (train, test) tuple of coo sparse matrices.
     """
     
+    idx = np.isnan(df['NotionalEUR'])
+    df = df.ix[~idx, :]
     ticker_idx = {tck: i for i, tck in enumerate(set(df['Ticker']))}
-    df['TradeDate'] = pd.to_datetime(df['TradeDate'], format='%Y%m%d')
-    train = df.ix[df[u'TradeDate'] <= cut_date, :]
-    test = df.ix[df[u'TradeDate'] > cut_date, :]
+    df['TradeDateKey'] = pd.to_datetime(df['TradeDateKey'], format='%Y%m%d')
+    train = df.ix[df[u'TradeDateKey'] <= cut_date, :]
+    test = df.ix[df[u'TradeDateKey'] > cut_date, :]
     
-    ddata = df.ix[df["BuySell"] == "Buy", [u'Customer', u'NotionalEUR', u'Ticker', u'TradeDate']]
+    ddata = df.ix[df["BuySell"] == "Buy", [u'CustomerNameIdx', u'NotionalEUR', u'Ticker', u'TradeDateKey']]
 
     data_train = pd.DataFrame.copy(ddata)
-    data_train.ix[data_train[u'TradeDate'] <= cut_date, "NotionalEUR"] = 0
+    data_train.ix[data_train[u'TradeDateKey'] <= cut_date, "NotionalEUR"] = 0
     
     data_test = pd.DataFrame.copy(ddata)
-    data_test.ix[data_test[u'TradeDate'] > cut_date, "NotionalEUR"] = 0
+    data_test.ix[data_test[u'TradeDateKey'] > cut_date, "NotionalEUR"] = 0
     
     # Bucket ratings into quantiles.
     cut_count = 10
     n_bins = 4
     labels = range(1, n_bins + 1)
     
-    train_dense = data_train.groupby(['Customer', 'Ticker'])
+    train_dense = data_train.groupby(['CustomerNameIdx', 'Ticker'])
     train_dense = (train_dense['NotionalEUR']
                    .agg({'NotionalSum' : np.sum, 'count' : 'count'})
                    .reset_index())
     train_dense = (train_dense
-                   .groupby('Customer')
+                   .groupby('CustomerNameIdx')
                    .filter(lambda x: sum(x['count']) >= cut_count))
-    train_dense[u'NotionalRating'], bins = pd.qcut(
-            train_dense[u'NotionalSum'], n_bins, labels=labels, retbins=True)
-    # train_dense[u'NotionalRating'] = train_dense[u'NotionalSum']
+#     train_dense[u'NotionalRating'], bins = pd.qcut(
+#             train_dense[u'NotionalSum'], n_bins, labels=labels, retbins=True)
+    train_dense[u'NotionalRating'] = train_dense[u'NotionalSum']
     
-    test_dense = data_test.groupby(['Customer', 'Ticker'])
+    test_dense = data_test.groupby(['CustomerNameIdx', 'Ticker'])
     test_dense = (test_dense['NotionalEUR']
                   .agg({'NotionalSum' : np.sum, 'count' : 'count'})
                   .reset_index())
-    test_dense = (test_dense.groupby('Customer')
+    test_dense = (test_dense.groupby('CustomerNameIdx')
                   .filter(lambda x: sum(x['count']) >= cut_count))
-    test_dense[u'NotionalRating'] = pd.cut(
-            test_dense[u'NotionalSum'], bins, labels=labels)
-    # test_dense[u'NotionalRating'] = test_dense[u'NotionalSum']
+#     test_dense[u'NotionalRating'] = pd.cut(
+#             test_dense[u'NotionalSum'], bins, labels=labels)
+    test_dense[u'NotionalRating'] = test_dense[u'NotionalSum']
     
     train_dense['Ticker'] = train_dense['Ticker'].map(lambda x: ticker_idx[x])
     test_dense['Ticker'] = test_dense['Ticker'].map(lambda x: ticker_idx[x])
@@ -73,20 +75,20 @@ def FormatAndSplit(df, cut_date):
     idx = ~np.isnan(np.array(test_dense['NotionalRating']))
     test_dense = test_dense[idx]
     
-    nb_customers = len(set(df['Customer']))
+    nb_CustomerNameIdxs = len(set(df['CustomerNameIdx']))
     nb_tickers = len(set(df['Ticker']))
     
     return (train, 
-            ToSparse(train_dense, nrow=nb_customers, ncol=nb_tickers),
+            ToSparse(train_dense, nrow=nb_CustomerNameIdxs, ncol=nb_tickers),
             test, 
-            ToSparse(test_dense, nrow=nb_customers, ncol=nb_tickers), 
+            ToSparse(test_dense, nrow=nb_CustomerNameIdxs, ncol=nb_tickers), 
             ticker_idx)
     
 
 def ToSparse(df, nrow, ncol):
     
     data = df['NotionalRating']
-    row = df['Customer']
+    row = df['CustomerNameIdx']
     col = df['Ticker']
     train_coo = coo_matrix((data, (row, col)), shape=(nrow, ncol))
     return train_coo
@@ -99,7 +101,7 @@ def GetLatentSpace(train, rank=10):
     M = np.array(mfit.coef())
     N = np.array(mfit.basis())
     
-    # GetLatentSpace customers
+    # GetLatentSpace CustomerNameIdxs
     M = M.T
     M_normalized = M / np.linalg.norm(M, axis=1)[:, np.newaxis]
     
@@ -110,15 +112,15 @@ def GetLatentSpace(train, rank=10):
     return M_normalized, N_normalized
 
 
-def GetClosestCustomers(ticker_id, train, rank=10):
+def GetClosestCustomers(ticker_id, train, rank=10, max_customers=20):
     
     M, N = GetLatentSpace(train)
     
     centroidN = N[ticker_id]
     
-    # Get closest customers
+    # Get closest CustomerNameIdxs
     d = M.dot(centroidN)
-    idx = d.argsort()[-20:][::-1]
+    idx = d.argsort()[-max_customers:][::-1]
     return idx
     
 
@@ -130,4 +132,3 @@ if __name__ == "__main__":
     train_df, train_coo, test_df, test_coo, ticker_idx = FormatAndSplit(df, cut_date=cut_date)
     cust = GetClosestCustomers(ticker_idx['GAZPRU'], train_coo, train_df, rank=10)  
     
-    print train.head()
